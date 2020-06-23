520b4b55a5c941158d85d1680701705ee50aa370
kunal8411 <63067292+kunal8411@users.noreply.github.com>
2020-06-23 16:05:43 +0530

    Create index.js

---
+++ Angular/Setting up a template Engine/index.js
@@ -0,0 +1,28 @@
+const express= require('express');
+const path= require('path');      //inbuild module
+const port=8000;
+
+
+
+
+const app= express();        //creating server using express........app will have many properties as a a key and value 
+app.set('view engine','ejs'); //setting view engine property in app object
+app.set('views',path.join(__dirname,'views'));     //__dirname gives us the correct path where our .js file is running and this set property
+                                                      will search views folder in that path, in views folder we have written our html file
+                                                      but with .ejs extansion 
+                                                        
+
+app.get('/',function(req,res){
+    
+    return res.render('home');  //whenevwe w render info from hrml file use render function and always return this one. 
+});
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log('error found',err);
+
+    }
+
+    console.log("Hello your server is running on port",port)
+})
